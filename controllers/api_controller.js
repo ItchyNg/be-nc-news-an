9386f6ee7f13e72fb9496b4756f863086426890d
ItchyNg <andyng06@hotmail.com>
2020-01-18 15:40:39 +0000

    .json

---
+++ controllers/api_controller.js
@@ -0,0 +1,7 @@
+// const apiDescription = require("endpoints.json");
+
+// console.log(apiDescription);
+
+// exports.getApiDescription = (req, res, next) => {
+//   res.status(200).send({ api: apiDescription });
+// };
