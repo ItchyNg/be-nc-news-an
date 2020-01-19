3ce5e54f579ea75e02ac72ea052cb71b3902b15f
ItchyNg <andyng06@hotmail.com>
2020-01-19 16:59:07 +0000

    json

--- routes/apiRouter.js
+++ routes/apiRouter.js
@@ -6,9 +6,10 @@ const commentsRouter = require("./commentsRouter");
 // const getApiDescription = require("../controllers/api_controller");
 const { send405Error } = require("../errors/index");
 const endpoints = require("../endpoints.json");
+const apiDescription = JSON.stringify(endpoints);
 
 apiRouter.get("/", function(req, res, next) {
-  res.status(200).send({ api: endpoints });
+  res.status(200).send({ api: apiDescription });
 });
 
 apiRouter.use("/topics", topicsRouter);
